5a28c61e300ced1ed7a2ec3df918865618484779
gabriel ros <gabriel.rosadjusting@gmail.com>
2021-02-11 18:33:21 -0500

    week2homework

---
+++ Week_2.js
@@ -0,0 +1,21 @@
+let gabrielBestPizza = "amazing";
+let numberOfToppings = 11;
+
+let pizza = ["ham ", "cheese ", "beef "];
+console.log(pizza);
+console.log(gabrielBestPizza);
+console.log(numberOfToppings);
+
+let pizzaPlace = "Gabriel's Best Pizza"
+console.log(`Welcome to ${pizzaPlace}. Try all ${pizza}!`);
+
+for (let i = 2; i <= 10; i+=2 ){
+  console.log(i);
+}
+if (numberOfToppings < 10 ){
+  console.log("quality, not quantity");
+}
+else  {
+console.log("whole lotta pizza");
+}
+console.log(typeof pizzaPlace);
